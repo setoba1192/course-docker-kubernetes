be355d955d53a0c83d5220f1737af023e0e81d6b
Joan <setoba1192@hotmail.com>
2023-01-10 23:12:17 -0500

    create base application

---
+++ course-kubernetes/ms-users/src/test/java/org/joan/springcloud/ms/users/msusers/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joan.springcloud.ms.users.msusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
