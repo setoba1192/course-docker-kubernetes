c463bb3de61a48d30e894bec888dd9ccbad706d6
Joan <setoba1192@hotmail.com>
2023-01-11 22:26:03 -0500

    add service layer for User entity

---
+++ course-kubernetes/ms-users/src/main/java/org/joan/springcloud/ms/users/service/UserService.java
@@ -0,0 +1,17 @@
+package org.joan.springcloud.ms.users.service;
+
+import org.joan.springcloud.ms.users.model.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    List<User> list();
+
+    Optional<User> findById(Long id);
+
+    User save(User user);
+
+    void delete(Long id);
+}
---
+++ course-kubernetes/ms-users/src/main/java/org/joan/springcloud/ms/users/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package org.joan.springcloud.ms.users.service;
+
+import org.joan.springcloud.ms.users.model.entity.User;
+import org.joan.springcloud.ms.users.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> list() {
+        return (List<User>) this.userRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<User> findById(Long id) {
+        return this.userRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public User save(User user) {
+        return this.userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
