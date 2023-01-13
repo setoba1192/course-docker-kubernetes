2703601b10ae2f61d3421f142ed0890df4ee81a8
Joan <setoba1192@hotmail.com>
2023-01-13 12:35:53 -0500

    add save, update and delete method to CourseController

--- course-kubernetes/ms-courses/src/main/java/com/joan/springcloud/ms/courses/mscourses/controller/CourseController.java
+++ course-kubernetes/ms-courses/src/main/java/com/joan/springcloud/ms/courses/mscourses/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.joan.springcloud.ms.courses.mscourses.controller;
 import com.joan.springcloud.ms.courses.mscourses.entity.Course;
 import com.joan.springcloud.ms.courses.mscourses.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,5 +28,25 @@ public class CourseController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.courseService.save(course));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> update(@RequestBody Course course, @PathVariable Long id) {
+        return this.courseService.findById(id).map(c -> {
+            c.setName(course.getName());
+            return ResponseEntity.ok(courseService.save(c));
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return this.courseService.findById(id).map(c -> {
+            this.courseService.delete(c.getId());
+            return ResponseEntity.noContent().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 }
