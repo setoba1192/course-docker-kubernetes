d18bcfde492692f4afdc655c5bb3319cf813668f
Joan <setoba1192@hotmail.com>
2023-01-12 23:52:56 -0500

    add module courses microservice

---
+++ course-kubernetes/ms-courses/src/main/java/com/joan/springcloud/ms/courses/mscourses/MsCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.joan.springcloud.ms.courses.mscourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCoursesApplication.class, args);
+	}
+
+}
---
+++ course-kubernetes/ms-courses/src/test/java/com/joan/springcloud/ms/courses/mscourses/MsCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joan.springcloud.ms.courses.mscourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
