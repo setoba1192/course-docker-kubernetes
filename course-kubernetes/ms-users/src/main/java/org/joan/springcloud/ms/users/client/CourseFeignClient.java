b90a6d92d1b2ccae8a68366928a56fc3ea82220b
Joan <setoba1192@hotmail.com>
2023-02-05 22:18:10 -0500

    add dependecies kubernetes client
    add kubernetes client and add configuration in properties to enable kubernetes client and general configuration

--- course-kubernetes/ms-users/src/main/java/org/joan/springcloud/ms/users/client/CourseFeignClient.java
+++ course-kubernetes/ms-users/src/main/java/org/joan/springcloud/ms/users/client/CourseFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "ms-courses", url = "${ms.courses.url}")
+@FeignClient(value = "ms-courses")
 public interface CourseFeignClient {
 
     @DeleteMapping("/delete-course-user/{userId}")
