c255aca7eb3e8b9141080d32aa91be30c7f654ea
Joan <setoba1192@hotmail.com>
2023-02-12 21:18:07 -0500

    add securityconfig and security dependencies

--- course-kubernetes/ms-auth/src/main/java/org/joan/springcloud/ms/auth/MsAuthApplication.java
+++ course-kubernetes/ms-auth/src/main/java/org/joan/springcloud/ms/auth/MsAuthApplication.java
@@ -1,4 +1,4 @@
-package org.joan.springcloud.ms.msauth;
+package org.joan.springcloud.ms.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
