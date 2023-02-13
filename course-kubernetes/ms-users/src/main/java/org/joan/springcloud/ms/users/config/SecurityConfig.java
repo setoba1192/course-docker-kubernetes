1d9d641159723313d72fd88692204b9a42c1490b
Joan <setoba1192@hotmail.com>
2023-02-12 21:49:49 -0500

    secure resource in ms-users module

--- course-kubernetes/ms-users/src/main/java/org/joan/springcloud/ms/users/config/SecurityConfig.java
+++ course-kubernetes/ms-users/src/main/java/org/joan/springcloud/ms/users/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.joan.springcloud.ms.users.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,12 +14,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().anyRequest().authenticated()
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/authorized").permitAll()
+                .requestMatchers(HttpMethod.GET, "/", "/{id}").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+                .requestMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
+                .requestMatchers(HttpMethod.PUT, "/{id}").hasAuthority("SCOPE_write")
+                .requestMatchers(HttpMethod.DELETE, "/{id}").hasAuthority("SCOPE_write")
+                .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/ms-users-client"))
-                .oauth2Client(withDefaults());
+                .oauth2Client(withDefaults())
+                .oauth2ResourceServer().jwt();
 
         return httpSecurity.build();
     }
